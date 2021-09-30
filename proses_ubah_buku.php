d8b76fb50541de8ae559eac0f02dabc921b9c00d
nafi-36 <75818386+nafi-36@users.noreply.github.com>
2021-09-30 10:03:43 +0700

    Update proses_ubah_buku.php

--- proses_ubah_buku.php
+++ proses_ubah_buku.php
@@ -6,27 +6,33 @@
     $pengarang = $_POST['pengarang'];
     $deskripsi = $_POST['deskripsi'];
 
-    $rand = rand();
-    $ekstensi =  array('png','jpg','jpeg');
+    $temp = $_FILES['foto']['tmp_name'];
     $name = $_FILES['foto']['name'];
-    $ukuran = $_FILES['foto']['size'];
-    $ext = pathinfo($name, PATHINFO_EXTENSION);
+    $type = $_FILES['foto']['type'];
+    $size = $_FILES['foto']['size'];
+    $newname = rand(0,9999).'_'.$name;
+    $folder = "gambar/";
 
-    if(!in_array($ext, $ekstensi)) {
-        echo "<script>alert('Ekstensi tidak sesuai');location.href='tampil_buku.php';</script>";
-    } else {
-        if ($ukuran <= 2000000) {	
-            $newname = $rand.'_'.$name;	
-            move_uploaded_file($_FILES['foto']['tmp_name'], 'gambar/'.$newname);
-            $ubah = mysqli_query($koneksi, "UPDATE buku SET nama_buku = '".$nama_buku."', pengarang = '".$pengarang."',
-                    deskripsi = '".$deskripsi."', foto = '".$newname."' WHERE id_buku = '{$id_buku}'");
-            if ($ubah) {
-                echo "<script>alert('Berhasil mengubah buku');location.href='tampil_buku.php';</script>";
-            } else {
-                echo "<script>alert('Gagal mengubah buku');location.href='ubah_buku.php?id_buku=".$id_buku."';</script>";
-            }        
+    $sql = "SELECT * FROM buku WHERE id_buku = '$id_buku'";
+    $query = mysqli_query($koneksi, $sql);
+    $buku = mysqli_fetch_array($query);
+    $path = $folder.$buku["foto"];
+
+    if (file_exists($path)) {
+        unlink($path);
+    }
+
+    if ($size <= 2000000 and ($type == "image/jpg" or $type == "image/png" or $type == "image/jpeg")) {
+        move_uploaded_file($temp, $folder.$newname);
+        $sql = "UPDATE buku SET nama_buku = '".$nama_buku."', pengarang = '".$pengarang."',
+               deskripsi = '".$deskripsi."', foto = '".$newname."' WHERE id_buku = '$id_buku'";
+        $result = mysqli_query($koneksi, $sql);
+        if ($result) {
+            echo "<script>alert('Berhasil menambahkan buku');location.href='tampil_buku.php';</script>";
         } else {
-            echo "<script>alert('Ukuran foto melebihi 2 MB');location.href='tampil_buku.php';</script>";
+            echo "<script>alert('Gagal menambahkan buku');location.href='tampil_buku.php';</script>";
         }
-    }    
+    } else {
+        echo "<script>alert('File tidak sesuai');location.href='tambah_buku.php';</script>";
+    }
 ?>
