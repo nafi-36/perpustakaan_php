9b34be1cd31c5da4863e3cd4e1c3ffec828b10df
nafi-36 <75818386+nafi-36@users.noreply.github.com>
2021-10-01 08:36:25 +0700

    Update hapus_buku.php

--- hapus_buku.php
+++ hapus_buku.php
@@ -1,9 +1,22 @@
 <?php
     if ($_GET['id_buku']) {
         include "koneksi.php";
-        $qry_hapus = mysqli_query($koneksi, "DELETE FROM buku WHERE id_buku = '".$_GET['id_buku']."'");
+        
+        $id_buku = $_GET['id_buku'];
+        $folder = "gambar/";
+
+        $sql = "SELECT * FROM buku WHERE id_buku = '$id_buku'";
+        $query = mysqli_query($koneksi, $sql);
+        $buku = mysqli_fetch_array($query);
+        $path = $folder.$buku["foto"];
+        
+        if (file_exists($path)) {
+            unlink($path);
+        }
+
+        $qry_hapus = mysqli_query($koneksi, "DELETE FROM buku WHERE id_buku = '$id_buku'");
         if ($qry_hapus) {
-            echo "<script>alert('Sukses menghapus buku');location.herf='tampil_buku.php';</script>";
+            echo "<script>alert('Berhasil menghapus buku');location.herf='tampil_buku.php';</script>";
         } else {
             echo "<script>alert('Gagal menghapus buku');location.href='tampil_buku.php';</script>";
         }
