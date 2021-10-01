9ea36e3bdac02d876b45d697511e8f672c0eb170
nafi-36 <75818386+nafi-36@users.noreply.github.com>
2021-10-01 10:07:45 +0700

    Update tambah_kelas.php

--- tambah_kelas.php
+++ tambah_kelas.php
@@ -15,7 +15,7 @@
         <div class="card" style="margin: 20px;">
             <div class="card-header"><h1>Tambah Kelas</h1></div>
             <div class="card-body">
-                <a class="btn btn-secondary" href="perpus.html" role="button">Kembali ke menu awal</a>
+                <a class="btn btn-secondary" href="tampil_kelas.php" role="button">Kembali ke Data Kelas</a>
                 <hr>
                 <form method="POST" action="proses_tambah_kelas.php">
                     <div class="mb-2">
