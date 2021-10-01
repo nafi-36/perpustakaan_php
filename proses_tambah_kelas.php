4fde35e9a296d8dbd4b7dbb778d4b62853d4e694
nafi-36 <75818386+nafi-36@users.noreply.github.com>
2021-10-01 11:36:21 +0700

    Update proses_tambah_kelas.php

--- proses_tambah_kelas.php
+++ proses_tambah_kelas.php
@@ -5,7 +5,7 @@
     include "koneksi.php";
     $input = mysqli_query($koneksi, "INSERT INTO kelas (nama_kelas, kelompok) VALUES ('{$nama_kelas}', '{$kelompok}')");
     if ($input) {
-        echo "<script>alert('Berhasil');location.href='tambah_kelas.php';</script>";
+        echo "<script>alert('Berhasil');location.href='tampil_kelas.php';</script>";
     }
     else {
         echo "<script>alert('Gagal');location.href='tambah_kelas.php';</script>";
